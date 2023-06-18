503662e52e6cd9c3d99132ea7560dbf7f719ee6b
Iseath <ethanalfasi@hotmail.com>
2023-06-19 00:08:17 +0100

    Add project files.

---
+++ Card.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Drawing;
+
+namespace Trio_Solitaire
+{
+    class Card
+    {
+        private Guid cardId;
+        private Bitmap frontFace { get; set; }
+        private Bitmap backFace { get; set; }
+
+        private bool whichSide { get; set; } = false;   //false is back, true is front
+
+        private int rank { get; set; } //0-13, ace < 2 < 3 < 4 ... 10 < jack < queen < king
+
+        private string suit { get; set; }
+
+        public Card (int cardRank, string cardSuit) 
+        {
+            cardId = new Guid();
+            rank = cardRank;
+            suit = cardSuit;
+
+        }
+
+        public void flip()
+        {
+            this.whichSide = !whichSide;
+        }
+    }
+}
---
+++ Deck.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Trio_Solitaire
+{
+    class Deck
+    {
+        private Card[] cards { get; set; } = new Card[52];
+
+        public  Deck() 
+        {
+            string cardSuit;
+            for (int i = 0; i < 4; i++)
+            {
+                switch (i)
+                {
+                    default:
+                        cardSuit = "Hearts";
+                        break;
+                    case 1:
+                        cardSuit = "Diamonds";
+                        break;
+                    case 2:
+                        cardSuit = "Clubs";
+                        break;
+                    case 3:
+                        cardSuit = "Spades";
+                        break;
+                }
+                for (int j = 0; j < 13; j++)
+                {
+                    Card add = new Card(j, cardSuit);
+                    cards.Append<Card>(add);
+                }
+
+            }
+        }
+
+        public void shuffle() 
+        {
+
+        }
+
+        public void deal() 
+        {
+
+        }
+
+    }
+}
