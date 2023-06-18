808cb7074181abf6016f2062e2a9fc59f8202b55
Iseath <ethanalfasi@hotmail.com>
2023-06-19 00:21:43 +0100

    added readme

--- Card.cs
+++ Card.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.IO;
 using System.Drawing;
+using System.Windows.Forms;
 
 namespace Trio_Solitaire
 {
@@ -14,18 +15,25 @@ namespace Trio_Solitaire
         private Bitmap frontFace { get; set; }
         private Bitmap backFace { get; set; }
 
+        private PictureBox image { get; set; }
+
         private bool whichSide { get; set; } = false;   //false is back, true is front
 
         private int rank { get; set; } //0-13, ace < 2 < 3 < 4 ... 10 < jack < queen < king
 
         private string suit { get; set; }
 
-        public Card (int cardRank, string cardSuit) 
+        public Card (int cardRank, string cardSuit, bool? side) 
         {
             cardId = new Guid();
             rank = cardRank;
             suit = cardSuit;
-
+            whichSide = side ?? whichSide;
+            /* TODO: 
+             * calculate filename for relevent card image from rank and suit
+             * use to fetch front and back
+             * set image to whatever side currently set
+             */
         }
 
         public void flip()
---
+++ readme.txt
@@ -0,0 +1,7 @@
+﻿Hello!
+
+This is just a cute little Solitaire client I'm making for a couple of friends, featuring all 3 of their illustrious baby boys.
+
+Suggestions welcome! This is just a hacky little home project, so I'm not putting *too* much effort into it, but I'm always happy to see what people have to say about my code.
+
+god bless
